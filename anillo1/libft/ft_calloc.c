a304e5ae6dbef906a5486f31c3194632a62f5d48
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-09-25 18:19:14 +0200

    Update ft_calloc.c

--- anillo1/libft/ft_calloc.c
+++ anillo1/libft/ft_calloc.c
@@ -13,11 +13,14 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	//declaracion de un puntero tipo void
 	void	*ptr;
 
+	//utilizacion de MALLOC para asignar dinamicamente un bloque de memoria de tamaño count * size y se almacene en el puntero ptr.
 	ptr = malloc(count * size);
 	if (ptr == NULL)
 		return (ptr);
+	//se utiliza la funcion ft_bzero para establecer todos los bytes en el bloque de memoria recien asignado a cero.
 	ft_bzero(ptr, size * count);
 	return (ptr);
 }
