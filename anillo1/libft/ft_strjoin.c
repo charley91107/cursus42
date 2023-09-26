61acbdaafd8070b2fa1cb7d7aa2bcbe60091a563
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-09-26 19:59:13 +0200

    Update ft_strjoin.c

--- anillo1/libft/ft_strjoin.c
+++ anillo1/libft/ft_strjoin.c
@@ -23,10 +23,19 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	lens1 = ft_strlen(s1);
 	lens2 = ft_strlen(s2);
 	totallen = lens1 + lens2;
+	/*
+ 	Se asigna memoria dinámica para la cadena resultante substring
+	*/
 	substring = (char *)malloc((totallen + 1) * sizeof(char));
 	if (substring == NULL)
 		return (NULL);
+	/*
+	Se copia la cadena s1 en substring utilizando ft_strlcpy
+	*/
 	ft_strlcpy(substring, s1, lens1 + 1);
+	/*
+ 	Se copia la cadena s2 en substring justo después de la primera cadena, 
+  	*/
 	ft_strlcpy(substring + lens1, s2, lens2 + 1);
 	return (substring);
 }
