00ba108a0bac48cd5b271e174ed276b0d1f057e0
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-09-26 19:35:59 +0200

    Update ft_strchr.c

--- anillo1/libft/ft_strchr.c
+++ anillo1/libft/ft_strchr.c
@@ -19,12 +19,23 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i])
 	{
+		/*
+  		Comprueba si el carácter actual en la cadena s es igual al carácter c que estamos buscando.
+    		*/
 		if (s[i] == (char)c)
+			/*
+			Si el carácter c se encuentra en la cadena s, devuelve un puntero al lugar en 
+			la cadena donde se encuentra ese carácter.
+			*/
 			return ((char *)&s[i]);
 		i++;
 	}
+	//se realiza una comprobación adicional para verificar si el último carácter de s es igual a c. 
 	if (s[i] == (char)c)
 		return ((char *)&s[i]);
+	/*se realiza una comprobación especial para el caso en el que c sea igual a '\0', 
+	es decir, el carácter nulo. En este caso, devuelve un puntero al final de la cadena s.
+	*/
 	if (c == '\0')
 		return ((char *)&s[i]);
 	return (NULL);
