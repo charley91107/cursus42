cd6799e81650cfa35e39d8bc7b57dbb9673501c6
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-09-26 20:31:17 +0200

    Update ft_strncmp.c

--- anillo1/libft/ft_strncmp.c
+++ anillo1/libft/ft_strncmp.c
@@ -18,6 +18,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n && s1[i] && s2[i])
 	{
+		/*
+		if (s1[i] != s2[i]): Se compara el carácter actual de s1 con el carácter actual de s2. 
+		Si son diferentes, Devuelve la diferencia entre los valores numéricos de los caracteres. 
+		El uso de (unsigned char) asegura que los caracteres se interpreten como números sin signo.
+		*/
 		if (s1[i] != s2[i])
 		{
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
