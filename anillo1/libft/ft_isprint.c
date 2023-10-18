0eaf719f5c79d7e6527249234f87c4ff355ac291
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-18 15:09:07 +0200

    Update ft_isprint.c

--- anillo1/libft/ft_isprint.c
+++ anillo1/libft/ft_isprint.c
@@ -9,12 +9,19 @@
 /*   Updated: 2023/09/19 18:42:51 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+In this code, the function `ft_isprint` takes an integer `b` as input. It checks 
+if `b` is a printable character by comparing it with the ASCII values of the printable 
+characters (32 to 126). If `b` is a printable character, the function returns 1. Otherwise, it returns 0.
+*/
+
 #include "libft.h"
 
-int	ft_isprint(int b)
+int ft_isprint(int b)
 {
-	if (b >= 32 && b <= 126)
-		return (1);
-	else
-		return (0);
+    // Check if b is a printable character
+    if (b >= 32 && b <= 126)
+        return (1);
+    else
+        return (0);
 }
