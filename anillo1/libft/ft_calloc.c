cd449beb0752d5e307aa2db42eafc5c801037f87
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-18 13:25:55 +0200

    Update ft_calloc.c

--- anillo1/libft/ft_calloc.c
+++ anillo1/libft/ft_calloc.c
@@ -9,15 +9,27 @@
 /*   Updated: 2023/09/20 20:31:56 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+we define a function `ft_calloc` that takes two parameters, `count` and `size`. 
+The function allocates memory for an array of `count` elements, each of `size` bytes, 
+and initializes the memory to zero. The function returns a pointer to the allocated memory. 
+If the memory allocation fails, the function returns a null pointer.
+*/
+
 #include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
+	// Allocate memory
 	ptr = malloc(count * size);
 	if (ptr == NULL)
 		return (ptr);
-	ft_bzero(ptr, size * count);
+
+	// Initialize memory to zero
+	ft_bzero(ptr, count * size);
+
+	// Return allocated memory
 	return (ptr);
 }
