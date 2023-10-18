35eae5f53815ad34c30d1f88427e07c6ac5cd5d5
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-18 13:50:51 +0200

    Update ft_isalpha.c

--- anillo1/libft/ft_isalpha.c
+++ anillo1/libft/ft_isalpha.c
@@ -9,12 +9,18 @@
 /*   Updated: 2023/09/14 13:32:52 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+In this code, the function `ft_isalpha` takes an integer `str` as input. It checks if the input `str` 
+is an alphabet character (either lowercase or uppercase). If it is, the function returns 1. 
+Otherwise, it returns 0.
+*/
 #include "libft.h"
 
-int	ft_isalpha(int str)
+int ft_isalpha(int str)
 {
-	if ((str >= 'a' && str <= 'z') || (str >= 'A' && str <= 'Z'))
-		return (1);
-	else
-		return (0);
+    // Check if str is an alphabet character
+    if ((str >= 'a' && str <= 'z') || (str >= 'A' && str <= 'Z'))
+        return (1);
+    else
+        return (0);
 }
