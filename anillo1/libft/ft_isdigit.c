61a0877435170a827ab0dde9cf0f9b8224131621
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-18 14:03:38 +0200

    Update ft_isdigit.c

--- anillo1/libft/ft_isdigit.c
+++ anillo1/libft/ft_isdigit.c
@@ -9,12 +9,21 @@
 /*   Updated: 2023/09/12 15:58:20 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+n this code, the function `ft_isdigit` takes an integer `d` as input. It checks if the value of `d` 
+falls within the range of ASCII values for the digits 0-9. If it does, the function returns 1, 
+indicating that `d` is a digit. Otherwise, it returns 0, indicating that `d` is not a digit.
+*/
+
 #include "libft.h"
 
-int	ft_isdigit(int d)
+int ft_isdigit(int d)
 {
-	if (d >= '0' && d <= '9')
-		return (1);
-	else
-		return (0);
+    // Check if 'd' is a digit (0-9)
+    if (d >= '0' && d <= '9')
+        return (1); // It's a digit
+    else
+        return (0); // It's not a digit
 }
+
+
