f341ed05c635a27c4a1d19e0ab6b5111f27300f2
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-19 11:48:31 +0200

    Update ft_lstlast.c

--- anillo1/libft/ft_lstlast.c
+++ anillo1/libft/ft_lstlast.c
@@ -9,17 +9,26 @@
 /*   Updated: 2023/10/12 17:13:16 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+This code defines a function `ft_lstlast` that takes a pointer to a linked list as input and returns a 
+pointer to the last element of the list. The function uses a temporary pointer `tmp` to traverse the list. 
+If the list is not empty, the function enters a loop that continues until the end of the list. 
+Inside the loop, the temporary pointer is moved to the next element in the list. Finally, 
+the function returns the last element of the list, which is te element pointed to by the temporary pointer 
+at the end of the loop.*/                        
 #include "libft.h"
 
-t_list	*ft_lstlast(t_list *lst)
+// Function to get the last element of a linked list
+t_list *ft_lstlast(t_list *lst)
 {
-	t_list	*tmp;
+	t_list *tmp; // Initialize a temporary pointer
 
-	tmp = lst;
-	if (lst)
+	tmp = lst; // Set the temporary pointer to the start of the list
+	if (lst) // Check if the list is not empty
 	{
-		while (tmp -> next)
-			tmp = tmp -> next;
+		while (tmp -> next) // Loop until the end of the list
+			tmp = tmp -> next; // Move the temporary pointer to the next element
 	}
-	return (tmp);
+	return (tmp); // Return the last element of the list
 }
+
