49bb32f06012dd0825edb10e990b54297d9d1812
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-19 11:40:12 +0200

    Update ft_lstdelone.c

--- anillo1/libft/ft_lstdelone.c
+++ anillo1/libft/ft_lstdelone.c
@@ -9,11 +9,22 @@
 /*   Updated: 2023/10/12 17:06:55 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+In this code, we define a function `ft_lstdelone` that takes a pointer to a list node and a pointer 
+to a function as arguments. The function deletes the content of the list node using the provided function, 
+frees the memory allocated for the list node, and sets the list pointer to NULL.
+*/
+
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
+void ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	del (lst -> content);
-	free(lst);
-	lst = NULL;
+    // Delete content using provided function
+    del(lst->content);
+
+    // Free memory allocated for list node
+    free(lst);
+
+    // Set list pointer to NULL
+    lst = NULL;
 }
