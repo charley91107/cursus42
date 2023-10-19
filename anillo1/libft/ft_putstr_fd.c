6cf836044316c1b9d3a223495b494ae571aeac0d
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-19 13:22:09 +0200

    Update ft_putstr_fd.c

--- anillo1/libft/ft_putstr_fd.c
+++ anillo1/libft/ft_putstr_fd.c
@@ -9,17 +9,22 @@
 /*   Updated: 2023/10/01 16:18:33 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+La función imprime la cadena de caracteres en el descriptor de archivo proporcionado
+*/
+
 #include "libft.h"
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	/*
- 	Dentro del bucle, la función write se utiliza para escribir un byte (1 byte) del contenido apuntado 
-  	por s en el descriptor de archivo fd.
- 	*/
+	// Inicia un bucle while que recorre la cadena de caracteres hasta llegar al carácter nulo ('\0').
 	while (*s)
 	{
+		// Escribe el carácter actual de la cadena en el descriptor de archivo proporcionado.
 		write(fd, s, 1);
+		
+		// Mueve el puntero a la siguiente posición en la cadena.
 		s++;
 	}
 }
+
