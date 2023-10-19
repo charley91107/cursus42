2a4788e9b8551c69f770ddbf5f491dbf0a52440f
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-19 13:20:26 +0200

    Update ft_putnbr_fd.c

--- anillo1/libft/ft_putnbr_fd.c
+++ anillo1/libft/ft_putnbr_fd.c
@@ -13,25 +13,36 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	// Verifica si el número es el límite inferior de los enteros de 32 bits (-2147483648)
 	if (n == -2147483648)
 	{
+		// Imprime el signo negativo y el dígito '2' para manejar el límite inferior
 		ft_putchar_fd('-', fd);
 		ft_putchar_fd('2', fd);
+		// Cambia el valor de n para evitar desbordamiento al convertir a positivo
 		n = 147483648;
 	}
+
+	// Maneja números negativos
 	if (n < 0)
 	{
+		// Imprime el signo negativo y convierte n a positivo para procesarlo
 		ft_putchar_fd('-', fd);
 		n *= -1;
 	}
+
+	// Caso base: si el número es un solo dígito, imprímelo y devuelve
 	if (n < 10)
 	{
-		ft_putchar_fd((n + 48), fd);
+		// Imprime el dígito y devuelve la función
+		ft_putchar_fd((n + 48), fd); // 48 es el valor ASCII de '0'
 		return ;
 	}
 	else
 	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		// Si el número tiene más de un dígito, imprime recursivamente los dígitos
+		ft_putnbr_fd(n / 10, fd); // Imprime el cociente dividiendo n por 10
+		ft_putnbr_fd(n % 10, fd); // Imprime el residuo (último dígito)
 	}
 }
+
