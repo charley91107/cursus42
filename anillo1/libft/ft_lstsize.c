24672ea76c0cc8b16e1b9de5fa452734b81d9b2b
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-19 12:05:50 +0200

    Update ft_lstsize.c

--- anillo1/libft/ft_lstsize.c
+++ anillo1/libft/ft_lstsize.c
@@ -9,8 +9,16 @@
 /*   Updated: 2023/10/12 13:30:29 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+In this code, the `ft_lstsize` function takes a pointer to a linked list as input and returns the size 
+of the list. The size of the list is determined by counting the number of nodes in the list. The function 
+uses a while loop to iterate through the list until it reaches the end. The size of the list is stored in 
+the variable `i`. Finally, the function returns the size of the list.
+*/
+
 #include "libft.h"
 
+// Function to get list size
 int	ft_lstsize(t_list *lst)
 {
 	size_t	i;
