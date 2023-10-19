92f68976f1bc5ebe4c6777d5e43a860e0b813898
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-19 12:53:58 +0200

    Update ft_memset.c

--- anillo1/libft/ft_memset.c
+++ anillo1/libft/ft_memset.c
@@ -9,19 +9,27 @@
 /*   Updated: 2023/09/13 21:11:04 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+La función ft_memset se utiliza para llenar un bloque de memoria con un valor específico.
+*/
+
 #include "libft.h"
 
-void	*ft_memset(void *str2, int f, size_t n)
+void *ft_memset(void *str2, int f, size_t n)
 {
-	unsigned char	*aux;
-	size_t			i;
+    unsigned char *aux; // Declara un puntero a caracteres sin signo
+    size_t i; // Declara una variable para contar iteraciones
 
-	aux = str2;
-	i = 0;
-	while (i < n)
-	{
-		aux[i] = (unsigned char)f;
-		i++;
-	}
-	return (str2);
+    aux = str2; // Asigna el puntero de entrada a aux para evitar cambiar str2
+    i = 0; // Inicializa el contador de iteraciones
+
+    // Itera sobre el bloque de memoria hasta alcanzar la longitud n
+    while (i < n)
+    {
+        aux[i] = (unsigned char)f; // Asigna el valor entero f al elemento actual del bloque de memoria
+        i++; // Incrementa el contador de iteraciones
+    }
+
+    return str2; // Devuelve un puntero al bloque de memoria modificado
 }
+
