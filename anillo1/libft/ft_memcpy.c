cae5eccb50c5257599f2c5256513d8f514ba44a1
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-19 12:28:42 +0200

    Update ft_memcpy.c

--- anillo1/libft/ft_memcpy.c
+++ anillo1/libft/ft_memcpy.c
@@ -9,25 +9,31 @@
 /*   Updated: 2023/09/19 22:21:52 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+La función memcpy se utiliza para copiar un bloque de memoria de una ubicación a otra
+*/
+
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void *ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t		i;
-	char		*dest_c;
-	const char	*src_c;
+    size_t      i;
+    char        *dest_c;
+    const char  *src_c;
 
-	src_c = src;
-	dest_c = dest;
-	i = 0;
-	while (i < n && (dest != NULL || src != NULL))
-	{
-		/*
-  		En cada iteración del bucle, un byte de datos se copia desde la ubicación de memoria apuntada por src_c 
-    		a la ubicación de memoria apuntada por dest_c.
-    		*/
-		dest_c[i] = src_c[i];
-		i++;
-	}
-	return (dest);
+    // Convierte los punteros de void a punteros de char para trabajar byte a byte.
+    src_c = src;
+    dest_c = dest;
+
+    i = 0;
+    // Itera hasta que se copien 'n' bytes o hasta que uno de los punteros sea NULL.
+    while (i < n && (dest != NULL || src != NULL))
+    {
+        // Copia un byte de la ubicación de memoria apuntada por src_c a dest_c.
+        dest_c[i] = src_c[i];
+        i++;
+    }
+    // Devuelve un puntero al destino (dest) después de copiar los datos.
+    return (dest);
 }
+
