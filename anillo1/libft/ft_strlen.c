50baaef94b3e9f9a62fd311bcbd364bfccdb5d98
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-21 19:38:54 +0200

    Update ft_strlen.c

--- anillo1/libft/ft_strlen.c
+++ anillo1/libft/ft_strlen.c
@@ -9,14 +9,26 @@
 /*   Updated: 2023/09/18 20:53:15 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+calcula la longitud de una cadena de caracteres (string). 
+*/
 #include "libft.h"
 
-size_t	ft_strlen(const char *e)
+size_t ft_strlen(const char *e)
 {
-	size_t	i;
-
-	i = 0;
-	while (e[i])
-		i++;
-	return (i);
+    // Se declara una variable 'i' de tipo size_t para mantener el contador de caracteres.
+    size_t i;
+    
+    // Se inicializa 'i' con 0 para contar los caracteres de la cadena.
+    i = 0;
+    
+    // Este bucle while recorre la cadena de caracteres hasta encontrar el carácter nulo '\0',
+    // que indica el final de la cadena.
+    while (e[i])
+        // Por cada caracter encontrado, incrementa el contador 'i'.
+        i++;
+    
+    // Finalmente, la función devuelve el valor de 'i', que representa la longitud de la cadena.
+    return (i);
 }
+
