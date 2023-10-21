651f9240d9cccfbf7b0026c0846d922983e6d265
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-21 20:15:12 +0200

    Update ft_strtrim.c

--- anillo1/libft/ft_strtrim.c
+++ anillo1/libft/ft_strtrim.c
@@ -9,27 +9,46 @@
 /*   Updated: 2023/09/24 13:11:20 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+elimina los caracteres especificados en el conjunto set del principio y del 
+final de la cadena s1.
+*/
+
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+char *ft_strtrim(char const *s1, char const *set)
 {
-	size_t	inicio;
-	size_t	final;
-	size_t	total;
-	char	*result;
+    size_t inicio;
+    size_t final;
+    size_t total;
+    char *result;
 
-	if (s1 == NULL || set == NULL)
-		return (NULL);
-	inicio = 0;
-	while (s1[inicio] && ft_strchr(set, s1[inicio]))
-		inicio++;
-	final = ft_strlen(s1);
-	while (final > inicio && ft_strchr(set, s1[final -1]))
-		final--;
-	total = final - inicio;
-	result = (char *)malloc((total + 1) * sizeof(char));
-	if (result == NULL)
-		return (NULL);
-	ft_strlcpy(result, s1 + inicio, total + 1);
-	return (result);
+    // Verifica si los punteros de entrada son nulos y devuelve NULL si es así
+    if (s1 == NULL || set == NULL)
+        return (NULL);
+
+    // Encuentra el índice del primer carácter que no está en el conjunto `set`
+    inicio = 0;
+    while (s1[inicio] && ft_strchr(set, s1[inicio]))
+        inicio++;
+
+    // Encuentra el índice del último carácter que no está en el conjunto `set`
+    final = ft_strlen(s1);
+    while (final > inicio && ft_strchr(set, s1[final - 1]))
+        final--;
+
+    // Calcula la longitud total del resultado después de eliminar los caracteres del conjunto `set`
+    total = final - inicio;
+
+    // Asigna memoria para el resultado más el carácter nulo al final
+    result = (char *)malloc((total + 1) * sizeof(char));
+    if (result == NULL)
+        return (NULL);
+
+    // Copia los caracteres relevantes de s1 al resultado
+    ft_strlcpy(result, s1 + inicio, total + 1);
+
+    // Devuelve el resultado
+    return (result);
 }
+
