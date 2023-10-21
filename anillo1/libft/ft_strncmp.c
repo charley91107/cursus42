10e1cd816aa4c430c68a6a57cd8c4994b89de8b4
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-21 20:02:01 +0200

    Update ft_strncmp.c

--- anillo1/libft/ft_strncmp.c
+++ anillo1/libft/ft_strncmp.c
@@ -9,23 +9,30 @@
 /*   Updated: 2023/09/19 21:27:40 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+que compara los primeros n caracteres de dos cadenas de texto (s1 y s2)
+*/
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+    size_t i; // Variable para iterar a través de los caracteres de las cadenas
 
-	i = 0;
-	while (i < n && s1[i] && s2[i])
-	{
-		if (s1[i] != s2[i])
-		{
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		}
-		i++;
-	}
-	while (i < n && ((s1[i] == '\0' && s2[i] != '\0')
-			|| (s1[i] != '\0' && s2[i] == '\0')))
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	return (0);
+    i = 0; // Inicializar el índice a 0
+    while (i < n && s1[i] && s2[i]) // Mientras no hayamos alcanzado el límite n y ambas cadenas no hayan terminado
+    {
+        if (s1[i] != s2[i]) // Si los caracteres actuales son diferentes
+        {
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]); // Devolver la diferencia entre los caracteres
+        }
+        i++; // Mover al siguiente carácter
+    }
+
+    // En este punto, al menos una cadena ha terminado o hemos alcanzado el límite n
+    while (i < n && ((s1[i] == '\0' && s2[i] != '\0') || (s1[i] != '\0' && s2[i] == '\0')))
+        // Mientras no hayamos alcanzado el límite n y una cadena ha terminado mientras la otra no
+        return ((unsigned char)s1[i] - (unsigned char)s2[i]); // Devolver la diferencia entre los caracteres
+
+    return (0); // Si las cadenas son iguales o hemos llegado al límite n, devolver 0
 }
+
