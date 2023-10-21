849ffe28cb879e580bb294e80ade64d7594c7543
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-21 18:21:52 +0200

    Update ft_strchr.c

--- anillo1/libft/ft_strchr.c
+++ anillo1/libft/ft_strchr.c
@@ -9,23 +9,27 @@
 /*   Updated: 2023/09/24 13:05:13 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+busca la primera aparición de un carácter dado (c) en una cadena de caracteres (s).
+*/
 
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
-	int	i;
+	int	i; // Se declara una variable entera para usar como índice en la cadena
 
-	i = 0;
-	while (s[i])
+	i = 0; // Se inicializa el índice en 0
+	while (s[i]) // Mientras no se llegue al final de la cadena (caracter nulo '\0')
 	{
-		if (s[i] == (char)c)
-			return ((char *)&s[i]);
-		i++;
+		if (s[i] == (char)c) // Si el carácter actual es igual al carácter que estamos buscando
+			return ((char *)&s[i]); // Devuelve un puntero al lugar donde se encontró el carácter
+		i++; // Incrementa el índice para pasar al siguiente carácter en la cadena
 	}
-	if (s[i] == (char)c)
-		return ((char *)&s[i]);
-	if (c == '\0')
-		return ((char *)&s[i]);
-	return (NULL);
+	if (s[i] == (char)c) // Si se encontró el carácter en la última posición de la cadena
+		return ((char *)&s[i]); // Devuelve un puntero a esa posición
+	if (c == '\0') // Si el carácter que estamos buscando es el carácter nulo ('\0')
+		return ((char *)&s[i]); // Devuelve un puntero al final de la cadena
+	return (NULL); // Si no se encuentra el carácter y no estamos buscando el carácter nulo, devuelve NULL
 }
+
