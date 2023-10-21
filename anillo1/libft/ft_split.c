0bdb46db0635e01261cef97929b19858a159fde5
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-21 18:06:44 +0200

    Update ft_split.c

--- anillo1/libft/ft_split.c
+++ anillo1/libft/ft_split.c
@@ -9,8 +9,14 @@
 /*   Updated: 2023/10/08 14:56:21 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/* 
+Este código es una implementación de la función ft_split que divide una cadena 
+(s) en palabras usando un delimitador dado (c). La función ft_split devuelve un 
+array de cadenas (un array de punteros a caracteres) donde cada cadena representa 
+una palabra en la cadena original.
+*/
 
-#include "libft.h"
+#include "libft.h" // Incluye el archivo de cabecera que contiene las declaraciones de las funciones utilizadas.
 
 static int	ft_count_word(char const *s, char c)
 {
@@ -19,18 +25,18 @@ static int	ft_count_word(char const *s, char c)
 
 	i = 0;
 	word = 0;
-	while (s && s[i])
+	while (s && s[i]) // Itera a través de la cadena de entrada mientras no haya llegado al final.
 	{
-		if (s[i] != c)
+		if (s[i] != c) // Si el carácter actual no es el delimitador, incrementa el contador de palabras.
 		{
 			word++;
-			while (s[i] != c && s[i])
+			while (s[i] != c && s[i]) // Avanza hasta encontrar el próximo delimitador o el final de la cadena.
 				i++;
 		}
 		else
-			i++;
+			i++; // Si el carácter actual es el delimitador, simplemente avanza al próximo carácter.
 	}
-	return (word);
+	return (word); // Devuelve el número total de palabras encontradas en la cadena.
 }
 
 static int	ft_size_word(char const *s, char c, int i)
@@ -38,19 +44,19 @@ static int	ft_size_word(char const *s, char c, int i)
 	int	size;
 
 	size = 0;
-	while (s[i] != c && s[i])
+	while (s[i] != c && s[i]) // Cuenta la longitud de la palabra actual hasta encontrar el delimitador o el final de la cadena.
 	{
 		size++;
 		i++;
 	}
-	return (size);
+	return (size); // Devuelve el tamaño de la palabra.
 }
 
 static void	ft_free(char **strs, int j)
 {
-	while (j-- > 0)
+	while (j-- > 0) // Libera cada cadena en el array de cadenas.
 		free(strs[j]);
-	free(strs);
+	free(strs); // Libera el array de punteros a cadenas.
 }
 
 char	**ft_split(char const *s, char c)
@@ -61,22 +67,23 @@ char	**ft_split(char const *s, char c)
 
 	i[0] = 0;
 	i[1] = -1;
-	word = ft_count_word(s, c);
-	strs = (char **)malloc((word + 1) * sizeof(char *));
-	if (!(strs))
+	word = ft_count_word(s, c); // Obtiene el número total de palabras en la cadena.
+	strs = (char **)malloc((word + 1) * sizeof(char *)); // Aloca memoria para el array de punteros a cadenas.
+	if (!(strs)) // Si la asignación de memoria falla, devuelve NULL.
 		return (NULL);
-	while (++i[1] < word)
+	while (++i[1] < word) // Itera a través del array de punteros a cadenas para llenarlo con las palabras.
 	{
-		while (s[i[0]] == c)
+		while (s[i[0]] == c) // Avanza hasta encontrar el primer carácter que no sea un delimitador.
 			i[0]++;
-		strs[i[1]] = ft_substr(s, i[0], ft_size_word(s, c, i[0]));
-		if (!(strs[i[1]]))
+		strs[i[1]] = ft_substr(s, i[0], ft_size_word(s, c, i[0])); // Crea una subcadena con la palabra actual y guárdala en el array.
+		if (!(strs[i[1]])) // Si la asignación de memoria falla, libera las cadenas anteriores y devuelve NULL.
 		{
 			ft_free(strs, i[1]);
 			return (NULL);
 		}
-		i[0] += ft_size_word(s, c, i[0]);
+		i[0] += ft_size_word(s, c, i[0]); // Avanza el índice para el próximo ciclo del bucle.
 	}
-	strs[i[1]] = 0;
-	return (strs);
+	strs[i[1]] = 0; // Establece el último puntero en el array como NULL para indicar el final del array de cadenas.
+	return (strs); // Devuelve el array de punteros a cadenas que contiene las palabras divididas.
 }
+
