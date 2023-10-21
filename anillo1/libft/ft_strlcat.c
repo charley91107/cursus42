6f8f17c761bc85d5c35eea84e4f68b165d21cf03
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-21 19:12:39 +0200

    Update ft_strlcat.c

--- anillo1/libft/ft_strlcat.c
+++ anillo1/libft/ft_strlcat.c
@@ -9,30 +9,48 @@
 /*   Updated: 2023/09/20 17:23:28 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+concatena cadenas de caracteres de origen (src) en la cadena de destino (dest) con un límite dado (n).
+*/
+
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t n)
+size_t ft_strlcat(char *dest, const char *src, size_t n)
 {
-	size_t	i;
-	size_t	j;
-	size_t	longdest;
-	size_t	longsrc;
+    size_t i;         // Variable de índice para recorrer la cadena de origen (src)
+    size_t j;         // Variable de índice para la posición de concatenación en dest
+    size_t longdest;  // Variable para almacenar la longitud de la cadena de destino (dest)
+    size_t longsrc;   // Variable para almacenar la longitud de la cadena de origen (src)
 
-	longdest = ft_strlen(dest);
-	longsrc = ft_strlen(src);
-	j = longdest;
-	i = 0;
-	if (longdest < n - 1 && n > 0)
-	{
-		while (src[i] && longdest + i < n - 1)
-		{
-			dest[j] = src[i];
-			j++;
-			i++;
-		}
-		dest[j] = '\0';
-	}
-	if (longdest >= n)
-		longdest = n;
-	return (longdest + longsrc);
-}	
+    // Calcula la longitud de la cadena de destino
+    longdest = ft_strlen(dest);
+    // Calcula la longitud de la cadena de origen
+    longsrc = ft_strlen(src);
+    // Inicializa el índice de la cadena de destino
+    j = longdest;
+    // Inicializa el índice de la cadena de origen
+    i = 0;
+
+    // Verifica si hay espacio suficiente para concatenar src en dest
+    if (longdest < n - 1 && n > 0)
+    {
+        // Copia los caracteres de src a dest hasta alcanzar el límite (n - 1)
+        // o hasta que se alcance el final de src
+        while (src[i] && longdest + i < n - 1)
+        {
+            dest[j] = src[i];
+            j++;
+            i++;
+        }
+        // Agrega el carácter nulo al final de la cadena resultante
+        dest[j] = '\0';
+    }
+
+    // Si la longitud de dest es mayor o igual a n, ajusta longdest a n
+    if (longdest >= n)
+        longdest = n;
+
+    // Retorna la longitud total de la cadena concatenada
+    return (longdest + longsrc);
+}
+	
