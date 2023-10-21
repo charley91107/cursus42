c1ba2f395b42e1c97dca15c49f8aca35b99a6aae
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-21 20:25:48 +0200

    Update ft_tolower.c

--- anillo1/libft/ft_tolower.c
+++ anillo1/libft/ft_tolower.c
@@ -9,10 +9,18 @@
 /*   Updated: 2023/09/18 19:48:28 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-int	ft_tolower(int c)
+int ft_tolower(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		c += 32;
-	return (c);
+    // Verifica si el valor de c representa una letra mayúscula en ASCII
+    if (c >= 'A' && c <= 'Z')
+    {
+        // Si es una letra mayúscula, suma 32 para convertirla en minúscula
+        c += 32;
+    }
+    // Retorna el valor de c, que puede ser el mismo si no es una letra mayúscula
+    // o el valor convertido a minúscula si es una letra mayúscula.
+    return (c);
 }
+
