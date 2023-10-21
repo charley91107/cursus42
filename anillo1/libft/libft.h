f2a3d61ea1dbc96646ecc5541d2aafdbeb1d2bf0
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-21 18:02:22 +0200

    Add files via upload

--- anillo1/libft/libft.h
+++ anillo1/libft/libft.h
@@ -6,15 +6,16 @@
 /*   By: cmunoz-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 17:38:40 by cmunoz-c          #+#    #+#             */
-/*   Updated: 2023/09/29 12:58:36 by cmunoz-c         ###   ########.fr       */
+/*   Updated: 2023/10/13 19:46:37 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include "stdio.h"
-# include "strings.h"
+# include "string.h"
 # include "stdlib.h"
+# include "unistd.h"
 
 int		ft_isalpha(int str);
 int		ft_isdigit(int d);
@@ -46,5 +47,23 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f) (unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
