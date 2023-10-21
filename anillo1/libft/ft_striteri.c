cbe1fd79686db06f369a92fb37649698da6908bd
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-21 18:36:47 +0200

    Update ft_striteri.c

--- anillo1/libft/ft_striteri.c
+++ anillo1/libft/ft_striteri.c
@@ -9,22 +9,28 @@
 /*   Updated: 2023/09/30 15:42:15 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+Esta función es útil cuando se quiere aplicar una operación específica a cada
+carácter de una cadena junto con su posición en la cadena. Por ejemplo, se podría 
+utilizar para modificar los caracteres en la cadena original según ciertas 
+condiciones o para realizar cualquier otra operación que involucre tanto el 
+carácter como su posición en la cadena.
+*/
+
 #include "libft.h"
 
+// La función ft_striteri toma una cadena de caracteres (s) y un puntero a una función (f)
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	unsigned int	i;
+	unsigned int	i; // Variable para almacenar el índice de la cadena
 
-	i = 0;
-	if (s == NULL || f == NULL)
-		return ;
-	/*
- 	dentro del bucle se llama a la funcion f pasando dos argumentos: el valor de i(indice del caracter de la cadena)
-  	y un puntero al caracter de la posicion i de la cadena s.
-  	*/
-	while (s[i])
+	i = 0; // Inicializa el índice en 0
+	if (s == NULL || f == NULL) // Verifica si la cadena o la función son nulas
+		return ; // Si alguna de las dos es nula, la función no hace nada y retorna inmediatamente
+
+	while (s[i]) // Itera a través de la cadena hasta encontrar el carácter nulo (fin de la cadena)
 	{
-		(*f)(i, &(s[i]));
-		i++;
+		(*f)(i, &(s[i])); // Llama a la función f con el índice actual y la dirección del carácter actual en la cadena
+		i++; // Incrementa el índice para pasar al siguiente carácter en la cadena
 	}
 }
