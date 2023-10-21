634827de13107a381ef8ceb5708dcae8a242af12
charley91107 <143663776+charley91107@users.noreply.github.com>
2023-10-21 19:45:49 +0200

    Update ft_strmapi.c

--- anillo1/libft/ft_strmapi.c
+++ anillo1/libft/ft_strmapi.c
@@ -9,30 +9,37 @@
 /*   Updated: 2023/09/27 17:01:38 by cmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+Toma una cadena de caracteres (s) y una función (f) como argumentos. La función 
+f se aplica a cada carácter de la cadena s, junto con su índice correspondiente, 
+para producir una nueva cadena de caracteres.
+*/
+
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	unsigned int	i;
-	char			*str
+    unsigned int    i;
+    char            *str;
 
-	/*
-	]se utiliza la funcion ft_srtdup para duplicar la cadena de entrada y se le asigna la moemoria necesaria 
-	para la nuvea cadena str
-	*/
-	str = ft_strdup(s);
-	if (str == NULL || f == NULL)
-		return (NULL);
-	/*
- 	bucle de mapeo, se entra en un bucle while que recoore cada caracter de la cadena de entrada s hasta llegar al caracter nulo.
-  	en cada iteraccion del bucle se aplica la funcion de mapeo f al caracter de la posicion i de la cadena str. el resultado de esta 
-   	operacion se le asigna de nuevo a str[i].
- 	*/
-	i = 0;
-	while (s[i])
-	{
-		str[i] = (*f)(i, str[i]);
-		i++;
-	}
-	return (str);
+    // Se reserva memoria para la nueva cadena usando la función ft_strdup.
+    str = ft_strdup(s);
+    
+    // Se verifica si la memoria se ha asignado correctamente y si la función es válida.
+    if (str == NULL || f == NULL)
+        return (NULL);
+
+    // Se itera a través de la cadena original s.
+    i = 0;
+    while (s[i])
+    {
+        // Se aplica la función f al carácter en la posición i y se guarda el resultado en str[i].
+        str[i] = (*f)(i, str[i]);
+        // Se mueve al siguiente carácter en la cadena.
+        i++;
+    }
+
+    // Se devuelve la nueva cadena modificada.
+    return (str);
 }
+
